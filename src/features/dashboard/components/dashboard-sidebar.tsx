b65537a8831fed1cb7a17d267aef486cb9b200cd
Gobi Mahato <thegobimahato@gmail.com>
2026-03-01 18:41:16 +0530

    feat(dashboard): add sidebar navigation layout

---
+++ src/features/dashboard/components/dashboard-sidebar.tsx
@@ -0,0 +1,216 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { OrganizationSwitcher, useClerk, UserButton } from "@clerk/nextjs";
+import {
+  AudioLines,
+  Headphones,
+  Home,
+  LayoutGrid,
+  type LucideIcon,
+  Settings,
+  Volume2,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface MenuItem {
+  title: string;
+  url?: string;
+  icon: LucideIcon;
+  onClick?: () => void;
+}
+
+interface NavSectionProps {
+  label?: string;
+  items: MenuItem[];
+  pathname: string;
+}
+
+function NavSection({ label, items, pathname }: NavSectionProps) {
+  return (
+    <SidebarGroup>
+      {label && (
+        <SidebarGroupLabel className="text-muted-foreground text-[13px] uppercase">
+          {label}
+        </SidebarGroupLabel>
+      )}
+
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                asChild={!!item.url}
+                isActive={
+                  item.url
+                    ? item.url === "/"
+                      ? pathname === "/"
+                      : pathname.startsWith(item.url)
+                    : false
+                }
+                onClick={item.onClick}
+                tooltip={item.title}
+                className="data-[active=true]:border-border h-9 border border-transparent px-3 py-2 text-[13px] font-medium tracking-tight data-[active=true]:shadow-[0px_1px_1px_0px_rgba(44,54,53,0.03),inset_0px_0px_0px_2px_white]"
+              >
+                {item.url ? (
+                  <Link href={item.url}>
+                    <item.icon /> <span>{item.title}</span>
+                  </Link>
+                ) : (
+                  <>
+                    <item.icon /> <span>{item.title}</span>
+                  </>
+                )}
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
+
+export default function DashboardSidebar() {
+  const pathname = usePathname();
+  const clerk = useClerk();
+
+  const mainMenuItems: MenuItem[] = [
+    {
+      title: "Dashboard",
+      url: "/",
+      icon: Home,
+    },
+    {
+      title: "Explore voices",
+      url: "/voices",
+      icon: LayoutGrid,
+    },
+    {
+      title: "Text to speech",
+      url: "/text-to-speech",
+      icon: AudioLines,
+    },
+    {
+      title: "Voice cloning",
+      icon: Volume2,
+    },
+  ];
+
+  const othersMenuItems: MenuItem[] = [
+    {
+      title: "Settings",
+      icon: Settings,
+      onClick: () => clerk.openOrganizationProfile(),
+    },
+    {
+      title: "Help and support",
+      url: "mailto:thegobimahato@gmail.com",
+      icon: Headphones,
+    },
+  ];
+  
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="flex flex-col gap-4 pt-4">
+        <div className="flex items-center gap-2 pl-1 group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:pl-0">
+          <Image
+            src={"/logo.png"}
+            alt="Resonance Logo"
+            width={24}
+            height={24}
+            className="rounded-sm"
+          />
+          <span className="text-foreground text-lg font-semibold tracking-tighter group-data-[collapsible=icon]:hidden">
+            Resonance
+          </span>
+
+          <SidebarTrigger className="ml-auto lg:hidden" />
+        </div>
+
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <OrganizationSwitcher
+              hidePersonal
+              fallback={
+                <Skeleton className="h-8.5 w-full rounded-md border bg-white group-data-[collapsible=icon]:size-8" />
+              }
+              appearance={{
+                elements: {
+                  rootBox:
+                    "w-full! group-data-[collapsible=icon]:w-auto! group-data-[collapsible=icon]:flex! group-data-[collapsible=icon]:justify-center!",
+                  organizationSwitcherTrigger:
+                    "w-full! justify-between! bg-white! border! border-border! rounded-md! pl-1! pr-2! py-1! gap-3! group-data-[collapsible=icon]:w-auto! group-data-[collapsible=icon]:p-1! shadow-[0px_1px_1.5px_0px_rgba(44,54,53,0.03)]!",
+                  organizationPreview: "gap-2!",
+                  organizationPreviewAvatarBox: "size-6! rounded-sm!",
+                  organizationPreviewTextContainer:
+                    "text-xs! tracking-tight! font-medium! text-foreground! group-data-[collapsible=icon]:hidden!",
+                  organizationPreviewMainIdentifier: "text-[13px]!",
+                  organizationSwitcherTriggerIcon:
+                    "size-4! text-sidebar-foreground! group-data-[collapsible=icon]:hidden!",
+                },
+              }}
+            />
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <div className="border-border border-b border-dashed" />
+
+      <SidebarContent>
+        <NavSection items={mainMenuItems} pathname={pathname} />
+        <NavSection
+          label="Others"
+          items={othersMenuItems}
+          pathname={pathname}
+        />
+      </SidebarContent>
+
+      <div className="border-border border-b border-dashed" />
+
+      <SidebarFooter className="gap-3 py-3">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <UserButton
+              showName
+              fallback={
+                <Skeleton className="border-border h-8.5 w-full rounded-md border bg-white group-data-[collapsible=icon]:size-8" />
+              }
+              appearance={{
+                elements: {
+                  rootBox:
+                    "w-full! group-data-[collapsible=icon]:w-auto! group-data-[collapsible=icon]:flex! group-data-[collapsible=icon]:justify-center!",
+                  userButtonTrigger:
+                    "w-full! justify-between! bg-white! border! border-border! rounded-md! pl-1! pr-2! py-1! shadow-[0px_1px_1.5px_0px_rgba(44,54,53,0.03)]! group-data-[collapsible=icon]:w-auto! group-data-[collapsible=icon]:p-1! group-data-[collapsible=icon]:after:hidden! [--border:color-mix(in_srgb,transparent,var(--clerk-color-neutral,#000000)_15%)]!",
+                  userButtonBox: "flex-row-reverse! gap-2!",
+                  userButtonOuterIdentifier:
+                    "text-[13px]! tracking-tight! font-medium! text-foreground! pl-0! group-data-[collapsible=icon]:hidden!",
+                  userButtonAvatarBox: "size-6!",
+                },
+              }}
+            />
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+
+      <SidebarRail />
+    </Sidebar>
+  );
+}
